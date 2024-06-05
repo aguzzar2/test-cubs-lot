92c6a0d51700624a22cc10701d49778a1e71737e
Anthony Guzzardo <aguzzar2@depaul.edu>
2024-06-05 11:02:41 -0500

    removed reservations

--- models/reservation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Reservation = require('./book')
-// Create Schema normal table database for Mongoose
-const authorSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true
-    }
-})
